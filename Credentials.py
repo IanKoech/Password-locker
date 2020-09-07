ff717fab3da43b2698b42cfe3adda82190a2bf60
Ian K <iankoech9@gmail.com>
2020-09-07 11:58:51 +0300

    Added methods and syntax to the Credentials class

--- Credentials.py
+++ Credentials.py
@@ -23,4 +23,21 @@ class Credentials:
         Credentials.Credentials_list.remove(self)
     
     @classmethod
-    def 
+    def generate_password(cls,length):
+        "Method generates password of a given length depending on user input"
+        #Length parameter determines the length of your password
+        letters=string.assii_lowercase
+        #Generating a password of a particular length
+        password="".join(random.choice(letters) for i in range(length))
+        return password
+
+    @classmethod 
+    def credentials_exist(cls,name):
+        """
+        Method checks if a particular user
+        has entered their credentials
+        """
+        for credentials in cls.Credentials_list:
+            if credentials.user_name=name:
+                return True
+        return False
